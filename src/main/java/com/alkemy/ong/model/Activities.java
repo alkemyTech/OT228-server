4e2419a7ab5cb25a2c55ff247300659cb33460dc
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2022-06-14 19:23:45 -0300

    Activities Model - Repository

---
+++ src/main/java/com/alkemy/ong/model/Activities.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+
+@Entity
+@Table(name = "activities")
+@Data
+@SQLDelete(sql = "UPDATE activities SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+public class Activities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, columnDefinition = "text")
+    private String content;
+    @Column(nullable = false)
+    private String image;
+    
+    private boolean deleted = Boolean.FALSE;
+
+    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+}
---
+++ src/main/java/com/alkemy/ong/repository/ActivitiesRepository.java
@@ -0,0 +1,10 @@
+
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Activities;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ActivitiesRepository extends JpaRepository<Activities, Long>{
+
+}
