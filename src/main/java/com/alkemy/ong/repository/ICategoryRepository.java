861938fd94a01734d1fc6bd3ed85ae16a6e180ca
Deivyd1995 <davideliasmoriello@gmail.com>
2022-06-16 12:29:15 -0300

    Añadida notacion Repository faltante en categorias.

--- src/main/java/com/alkemy/ong/repository/ICategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/ICategoryRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
 }
