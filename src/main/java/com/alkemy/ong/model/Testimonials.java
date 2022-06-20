333df0f5a55420b99de79e8f4cfc97a5373328a8
Valeria Riveros <92410347+ValRiv@users.noreply.github.com>
2022-06-20 20:25:13 -0300

    OT2J28-5  Create model and repository the Testimonials

---
+++ src/main/java/com/alkemy/ong/model/Testimonials.java
@@ -0,0 +1,62 @@
+package com.alkemy.ong.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "testimonials")
+@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
+public class Testimonials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "testimonials_id")
+    private Long id;
+
+    @NotNull(message = "Name must not be null.")
+
+    private String name;
+
+    private String image;
+
+    private String content;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    private boolean softDeleted = Boolean.FALSE;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Testimonials that = (Testimonials) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
+
---
+++ src/main/java/com/alkemy/ong/repository/TestimonialsRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Testimonials;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestimonialsRepository extends JpaRepository<Testimonials,Long> {
+
+}
