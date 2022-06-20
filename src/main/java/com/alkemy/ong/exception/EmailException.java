8805a00c89be773e1ae7a28f7ac9be0a0219b183
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2022-06-20 15:07:52 -0300

    Add EmailService SendGrid
    IEmailService
    EmailService
    EmailException
    Edit pom

---
+++ src/main/java/com/alkemy/ong/exception/EmailException.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.exception;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String message) {
+        super(message);
+    }
+
+    public EmailException() {
+    }
+
+}
