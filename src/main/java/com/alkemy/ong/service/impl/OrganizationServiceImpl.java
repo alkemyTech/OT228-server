10012737944c71486eeaf1ea72c7e3f643fbe6ac
Deivyd1995 <davideliasmoriello@gmail.com>
2022-06-22 20:42:11 -0300

    Añadida inyeccion de repositorio faltante en servicio de OrganizationImpl.

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.service.IOrganizationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,13 @@ import org.springframework.stereotype.Service;
 public class OrganizationServiceImpl  implements IOrganizationService {
 
     @Autowired
-    ObjectMapper objectMapper;
+   private ObjectMapper objectMapper;
+    @Autowired
+   private OrganizationRepository organizationRepository;
 
     @Override
     public OrganizationDto findById(Long id) {
-        OrganizationDto organizationFound = objectMapper.convertValue(OrganizationRepository.findById(id), OrganizationDto.class);
+        OrganizationDto organizationFound = objectMapper.convertValue(organizationRepository.findById(id), OrganizationDto.class);
         return organizationFound;
     }
 
