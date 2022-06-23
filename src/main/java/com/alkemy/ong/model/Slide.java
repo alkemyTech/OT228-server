fee249f0a5ad668f8b553a72c53792946aa3012c
FOMonti <montorofernando4315@gmail.com>
2022-06-23 10:48:49 -0300

    Created Slide Model and Repository

---
+++ src/main/java/com/alkemy/ong/model/Slide.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "slides")
+public class Slide {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @NotNull(message = "Content cannot be null")
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    @OneToOne
+    @NotNull(message = "Organization must not be null.")
+    @JoinColumn(name = "organization_id")
+    private Organization organization;
+}
--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -1,16 +1,18 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.service.IOrganizationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrganizationServiceImpl  implements IOrganizationService {
+public class OrganizationServiceImpl implements IOrganizationService {
 
     @Autowired
     ObjectMapper objectMapper;
+
 
     @Override
     public OrganizationDto findById(Long id) {
