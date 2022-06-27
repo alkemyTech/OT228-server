2386be1dd14a6960653599da0968430bd7708cd1
Roberto Ariel Latorraca <73842487+RobertoLatorraca@users.noreply.github.com>
2022-06-27 00:25:38 -0300

    Added regex check and BadRequestException.class. Feature/OT2J28-42

--- src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alkemy.ong.dto.CategoryDto;
+import com.alkemy.ong.exception.BadRequestException;
 import com.alkemy.ong.mappers.ModelMapperFacade;
 import com.alkemy.ong.model.Category;
 import com.alkemy.ong.repository.ICategoryRepository;
@@ -17,6 +18,7 @@ public class CategoryServiceImpl implements ICategoryService {
 	
 	@Override
 	public CategoryDto save(CategoryDto categoryDto) {
+        if (!categoryDto.getName().matches("^[a-zA-Z]*$")) throw new BadRequestException("The name only supports alphabetic characters.");
 		return ModelMapperFacade.map(
 				categoryRepository.save(ModelMapperFacade.map(
 						categoryDto, Category.class)),
