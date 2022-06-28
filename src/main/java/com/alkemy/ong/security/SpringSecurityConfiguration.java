434f9767386bfe8ab670e8629f13d98c205ebdeb
Samuel_Angel <sangelp@unal.edu.co>
2022-06-27 21:56:12 -0500

    Add Endpoint to delete Categories

--- src/main/java/com/alkemy/ong/security/SpringSecurityConfiguration.java
+++ src/main/java/com/alkemy/ong/security/SpringSecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 				// Public endpoints
 				.antMatchers(HttpMethod.POST, "/auth/**").permitAll()
 				// Private endpoints
-				.antMatchers("/categories").hasRole("ROLE_ADMIN")
+				.antMatchers("/categories/**").hasRole("ROLE_ADMIN")
 				.anyRequest().authenticated()
 				.and()
 		// Add JWT Token Filter
