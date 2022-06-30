067bdeec42c878e2bdede000c5fd53a51f667fdb
FOMonti <montorofernando4315@gmail.com>
2022-06-30 20:20:17 -0300

    Quitanto amazon properties

--- src/main/java/com/alkemy/ong/config/AwsConfig.java
+++ src/main/java/com/alkemy/ong/config/AwsConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:amazon.properties")
 public class AwsConfig {
 
     @Value("${aws.endpointUrl}")
