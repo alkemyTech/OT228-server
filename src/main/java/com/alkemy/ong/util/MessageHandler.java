36b6929c6ba11c7a65e5cd01461787a207b945f9
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2022-07-01 12:31:06 -0300

    Add Contact Email

--- src/main/java/com/alkemy/ong/util/MessageHandler.java
+++ src/main/java/com/alkemy/ong/util/MessageHandler.java
@@ -14,19 +14,29 @@ public class MessageHandler {
     public final String categoryNotFound;
     public final String newsNotFound;
     public final String categoryIdRequired;
+    public final String welcomeEmail;
+    public final String errorEmail;
+    public final String contactEmail;
+
 
     @Autowired
     public MessageHandler(@Value("${user.found}") String userFound,
     		@Value("${news.notFound}")String newsNotFound,
     		@Value("${category-id.required}") String categoryIdRequired,
     		@Value("${category.notFound}") String categoryNotFound,
-    		@Value ("${resource.notFound}") String resourceNotFound) {
+    		@Value ("${resource.notFound}") String resourceNotFound,
+                          @Value("${welcome.emailText}") String welcomeEmail,
+                          @Value("${error.emailText}") String errorEmail,
+                          @Value("${contact.emailText}") String contactEmail) {
 
         this.userFound = userFound;
         this.resourceNotFound= resourceNotFound;
         this.categoryNotFound= categoryNotFound;
         this.newsNotFound = newsNotFound;
         this.categoryIdRequired = categoryIdRequired;
+        this.welcomeEmail = welcomeEmail;
+        this.errorEmail = errorEmail;
+        this.contactEmail = contactEmail;
     }
 
 }
