1645b693c115c181201ad94eb8bf1900a6430d63
Samuel Angel Perez <sangelp@unal.edu.co>
2022-07-19 12:02:24 -0500

    Add Paging to ListMembers Endpoint

--- src/main/java/com/alkemy/ong/service/IMemberService.java
+++ src/main/java/com/alkemy/ong/service/IMemberService.java
@@ -3,12 +3,14 @@ package com.alkemy.ong.service;
 import java.util.List;
 
 import com.alkemy.ong.dto.MemberDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IMemberService {
 
 	MemberDto save(MemberDto memberDto);
 
-	List<MemberDto> findAll();
+	Page<MemberDto> findAll(Pageable pageable);
 
 	MemberDto update(MemberDto memberDto, Long id);
 
